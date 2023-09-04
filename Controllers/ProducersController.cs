f0bb6af0b98a200e09382a3b3c4c73d849048dab
mackblue786@gmail.com <mackblue786@gmail.com>
2023-09-04 15:50:42 +0500

    Add project files.

---
+++ Controllers/ProducersController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View(allProducers);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using eTickets.Data;
+using eTickets.Data.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// DbContext configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+//configuration services
+builder.Services.AddScoped<IActorsService, ActorsService>();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+//seed database
+AppDbInitializer.Seed(app);
+app.Run();
+
+
