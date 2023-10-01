f47538b6d7bc967d2af908e29788d8c05d9a81da
mackblue786@gmail.com <mackblue786@gmail.com>
2023-10-01 23:39:15 +0500

    #

--- Data/Services/IMoviesServices.cs
+++ Data/Services/IMoviesServices.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets.Data.Base;
+using eTickets.Data.ViewModels;
 using eTickets.Models;
 
 namespace eTickets.Data.Services
@@ -6,5 +7,8 @@ namespace eTickets.Data.Services
     public interface IMoviesServices : IEntityBaseRepository<Movie>
     {
         Task<Movie> GetMovieByIdAsync(int id);
+        Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
+        Task AddNewMovieAsync(NewMovieVM data);
+        Task UpdateMovieAsync(NewMovieVM data);
     }
 }
