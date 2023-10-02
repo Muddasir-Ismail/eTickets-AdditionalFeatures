46e5a614364542dbdde6a3bed7738cc843ed5e21
mackblue786@gmail.com <mackblue786@gmail.com>
2023-10-02 19:14:28 +0500

    #

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -21,6 +21,23 @@ namespace eTickets.Controllers
             return View(allMovies);
         }
 
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allMovies = await _service.GetAllAsync(n => n.Cinema);
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                //var filteredResult = allMovies.Where(n => n.Name.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+
+                var filteredResultNew = allMovies.Where(n => string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase) || string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+                return View("Index", filteredResultNew);
+            }
+
+            return View("Index", allMovies);
+        }
+
+
         //GET: Movies/Details/1
         public async Task<IActionResult> Details(int id)
         {
